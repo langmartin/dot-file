ec131b97a4ce2399a71ec4df8b8254adfbd92b0c
Lang Martin <lang.martin@gmail.com>
2016-11-16 15:55:31 -0500

    mu4e overhauled for gmail with labels

--- site-lisp/rc-mu4e.el
+++ site-lisp/rc-mu4e.el
@@ -83,89 +83,68 @@
   (define-key mu4e-view-mode-map "G" 'mu4e-view-go-to-url)
   (define-key mu4e-view-mode-map "g" 'mu4e-headers-search-bookmark)
 
-  (define-key mu4e-main-mode-map "l" 'mu4e~headers-jump-to-maildir)
-  (define-key mu4e-headers-mode-map "l" 'mu4e~headers-jump-to-maildir)
-  (define-key mu4e-view-mode-map "l" 'mu4e~headers-jump-to-maildir)
-
-  (define-key mu4e-headers-mode-map "y" 'mu4e-headers-mark-for-refile)
-  (define-key mu4e-view-mode-map "y" 'mu4e-view-mark-for-refile)
   (define-key mu4e-headers-mode-map "r" 'mu4e-compose-reply)
   (define-key mu4e-view-mode-map "r" 'mu4e-compose-reply)
 
-  (define-key mu4e-headers-mode-map "s" 'mu4e-headers-mark-move-to-starred)
-  (define-key mu4e-view-mode-map "s" 'mu4e-view-mark-move-to-starred)
-  (define-key mu4e-headers-mode-map "!" 'mu4e-headers-mark-move-to-junk)
-  (define-key mu4e-view-mode-map "!" 'mu4e-view-mark-move-to-junk)
-  (define-key mu4e-headers-mode-map "#" 'mu4e-headers-mark-for-trash)
-  (define-key mu4e-view-mode-map "#" 'mu4e-view-mark-for-trash)
+  (add-to-list
+   'mu4e-marks
+   '(tag
+     :char       "l"
+     :prompt     "label"
+     :ask-target (lambda () (read-string "label to add? "))
+     :action     (lambda (docid msg target)
+                   (mu4e-action-retag-message msg (concat "+" target)))))
 
-  (define-key mu4e-headers-mode-map "I" 'mu4e-headers-mark-for-read)
-  (define-key mu4e-headers-mode-map "U" 'mu4e-headers-mark-for-unread)
+  (add-to-list
+   'mu4e-marks
+   '(archive
+     :char       "y"
+     :prompt     "archive"
+     :show-target (lambda (target) "archive")
+     :action      (lambda (docid msg target)
+                    ;; must come before proc-move since retag runs
+                    ;; 'sed' on the file
+                    (mu4e-action-retag-message msg "-\\\\Inbox")
+                    (mu4e-action-retag-message msg "-\\\\Starred")
+                    (mu4e~proc-move docid nil "+S-u-N"))))
+
+  (mu4e~headers-defun-mark-for tag)
+  (mu4e~headers-defun-mark-for archive)
+  (define-key mu4e-headers-mode-map (kbd "l") 'mu4e-headers-mark-for-tag)
+  (define-key mu4e-view-mode-map (kbd "l") 'mu4e-headers-mark-for-tag)
+  (define-key mu4e-headers-mode-map (kbd "y") 'mu4e-headers-mark-for-archive)
+  (define-key mu4e-view-mode-map (kbd "y") 'mu4e-view-mark-for-archive)
 
   (setq
    mu4e-bookmarks
-   `((;; (concat "(maildir:/INBOX OR flag:unread)"
-      ;;          " AND NOT ("
-      ;;          "maildir:" mu4e-trash-folder
-      ;;          " OR "
-      ;;          "maildir:" mu4e-junk-folder ")")
-      "maildir:/INBOX" "Inbox" 105)
-     (,(concat "flag:flagged OR maildir:" mu4e-starred-folder) "Flagged" 115)
-     (,(concat "from:" user-mail-address " AND date:30d..now") "Last 30 days sent" 116)
-     (,(concat "flag:draft OR maildir:" mu4e-drafts-folder) "Drafts" 100)
+   `(("tag:\\\\Inbox" "Inbox" 105)
+     (,"tag:on-monday" "on Monday" 109)
+     (,"tag:on-first" "on The First" 102)
+     (,"tag:\\\\Starred" "Flagged" 115)
+     (,"tag:\\\\Draft" "Drafts" 100)
+     ;; (,(concat "from:" user-mail-address " AND date:30d..now")
+     ;;  "Last 30 days sent" 116)
+     ("tag:\\\\Sent AND date:30d..now" "Last 30 days sent" 116)
      ("date:7d..now" "Last 7 days" 97))))
 
-(rc-mu4e-gmail-shortcuts)
-
-
-;;; Bcc myself
-
-(add-hook
- 'mu4e-compose-mode-hook
- (defun rc-mu4e-bcc-myself ()
-   (save-excursion (message-add-header (concat "Bcc: " user-mail-address "\n")))))
-;; (add-hook 'mu4e-compose-mode-hook 'turn-on-visual-line-mode)
-;; (add-hook 'mu4e-compose-mode-hook 'turn-off-auto-fill)
-
-
-;;; Custom variables setup
-
-;; (setq mu4e-get-mail-command "offlineimap")
-;; (setq mu4e-get-mail-command "true")
-;; (setq mu4e-html2text-command "html2text -nobs -utf8")
-;; (setq mu4e-html2text-command "textutil -convert txt -stdin -stdout")
-;; (setq mu4e-html2text-command nil)
-
-(prog0
- (custom-set-variables
-  '(mu4e-refile-folder "/Archive")
-  '(mu4e-sent-folder "/Archive")
-  '(mu4e-trash-folder "/Deleted Items")
-  '(mu4e-drafts-folder "/Drafts")
-  '(mu4e-starred-folder "/Starred")
-  '(mu4e-junk-folder "/Junk Mail")
-  '(mu4e-get-mail-command "offlineimap"))
-
- (custom-set-variables
-  '(mu4e-refile-folder "/[Gmail].All Mail")
-  '(mu4e-sent-folder "/[Gmail].All Mail")
-  '(mu4e-trash-folder "/[Gmail].Trash")
-  '(mu4e-headers-skip-duplicates t)
-  '(mu4e-drafts-folder "/[Gmail].Drafts")
-  '(mu4e-starred-folder "/[Gmail].Starred")
-  '(mu4e-junk-folder "/[Gmail].Trash"))
-
- ;; minimal gmail setup, no all mail
- (custom-set-variables
-  '(mu4e-headers-skip-duplicates t)
-  '(mu4e-drafts-folder "/[Gmail].Drafts")
-  '(mu4e-starred-folder "/[Gmail].Starred")))
+(eval-after-load "mu4e"
+  '(progn
+     (rc-mu4e-gmail-shortcuts)
+     ;; bcc myself
+     (add-hook
+      'mu4e-compose-mode-hook
+      (defun rc-mu4e-bcc-myself ()
+        (save-excursion
+          (message-add-header
+           (concat "Bcc: " user-mail-address "\n")))))))
 
 (custom-set-variables
+ '(mu4e-headers-skip-duplicates t)
  '(mu4e-attachment-dir "~/Downloads")
  '(mu4e-date-format-long "%Y-%m-%d")
  '(mu4e-headers-date-format "%y-%m-%d")
- '(mu4e-get-mail-command "isync INBOX Archive Drafts 'Deleted Items'")
+ ;; '(mu4e-get-mail-command "isync INBOX Archive Drafts 'Deleted Items'")
+ '(mu4e-get-mail-command "offlineimap")
  '(mu4e-headers-fields (quote ((:human-date . 12) (:flags . 6) (:from . 22) (:subject))))
  '(mu4e-headers-leave-behavior (quote apply))
  '(mu4e-html2text-command "textutil -convert txt -stdin -stdout")
