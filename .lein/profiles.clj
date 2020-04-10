9dfb32515b2ab0f809488806cf876ab90d4ee8b3
Lang Martin <lang@hashicorp.com>
2020-04-10 13:10:29 -0400

    .lein/profiles: update tools, work with modern cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,33 +1,36 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.14.0"]
-            [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
+ {:plugins [;; [cider/cider-nrepl "0.24.0"]
+            [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
 
             ;; 1. jack in to a jvm repl
             ;; 2. (require '[figwheel-sidecar.repl-api :refer :all])
             ;; 3. (start-figwheel!)
             ;; 4. (cljs-repl)
-            [lein-figwheel "0.5.10" :exclusions [org.clojure/clojure]]
+            ;; [lein-figwheel "0.5.19" :exclusions [org.clojure/clojure]]
 
-            [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-            [venantius/yagni "0.1.4"]
-            [lein-kibit "0.1.5"]
-            [lein-bikeshed "0.4.1"]
+            [jonase/eastwood "0.3.11" :exclusions [org.clojure/clojure]]
+            [venantius/yagni "0.1.7"]
+            [lein-kibit "0.1.8"]
+            [lein-bikeshed "0.5.2"]
             ;; [lein-cloverage "1.0.7"]
             ]
 
-  :dependencies [[org.clojure/tools.trace "0.7.9"]
-                 [pjstadig/humane-test-output "0.8.1"]
-                 [org.clojure/tools.namespace "0.2.11"]
+  :dependencies [
+                 [org.clojure/clojure "1.10.1"]
+                 ;; [org.clojure/tools.trace "0.7.10"]
+                 ;; [pjstadig/humane-test-output "0.10.0"]
+                 ;; [org.clojure/tools.namespace "1.0.0"]
                  ;; need these for figwheel
-                 [figwheel-sidecar "0.5.10"]
-                 [com.cemerick/piggieback "0.2.1"]
+                 ;; [figwheel-sidecar "0.5.19"]
+                 [cider/piggieback "0.4.2"]
                  ]
 
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]}
+  ;; :injections [(require 'pjstadig.humane-test-output)
+  ;;              (pjstadig.humane-test-output/activate!)]
+  }
 
  :repl
  {:repl-options {:init (set! *print-length* 50)
-                 :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]
+                 :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
                  }
   }}
